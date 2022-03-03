f86358fdde83f02db66d1c792923821d9be04b26
Benson <48788513+benson00077@users.noreply.github.com>
2022-03-03 13:24:25 +0800

    Init users services

---
+++ src/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = {
+  id: number;
+  name: string;
+  username: string;
+  password: string;
+};
+
+@Injectable()
+export class UsersService {
+  // mock data from db
+  private readonly user: User[] = [
+    {
+      id: 1,
+      name: 'Ben',
+      username: 'Ben',
+      password: 'sth-like-bcrypt-hashed-pwd',
+    },
+    {
+      id: 2,
+      name: 'Ben2',
+      username: 'Ben2',
+      password: 'sth-like-bcrypt-hashed-pwd2',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.user.find(user => user.username === username)
+  }
+}

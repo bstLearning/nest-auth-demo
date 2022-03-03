c3a5ec40b6b9ae99bdecb9a31df87b10d0094cea
Benson <48788513+benson00077@users.noreply.github.com>
2022-03-03 14:04:11 +0800

    Set up passport strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [UsersService],
-  providers: [AuthService],
+  imports: [UsersService, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}

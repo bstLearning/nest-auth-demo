00747d7864897320b155cd37ff4ea14c7f1418fd
Benson <48788513+benson00077@users.noreply.github.com>
2022-03-03 14:32:35 +0800

    Fix wrong import of dependent module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
-import { UsersService } from 'src/users/users.service';
+import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [UsersService, PassportModule],
+  imports: [UsersModule, PassportModule],
   providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}

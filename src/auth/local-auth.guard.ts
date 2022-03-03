b4b05f354d05a766bceee7fbe81f90143f0e2ece
Benson <48788513+benson00077@users.noreply.github.com>
2022-03-03 15:09:41 +0800

    Add comments

--- src/auth/local-auth.guard.ts
+++ src/auth/local-auth.guard.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 
+// declaring useing passport local strategy
+
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {
   
